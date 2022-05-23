2aad2c10d54faaa9e2cb5d969fa6da37aef30fdf
haedo1221 <haedo1221@gmail.com>
2022-05-23 21:56:19 +0900

    11. [버그수정] 유저네임, 비밀번호 체크 완료

--- src/main/java/site/metacoding/blogv3/web/UserController.java
+++ src/main/java/site/metacoding/blogv3/web/UserController.java
@@ -5,25 +5,23 @@ import java.util.Map;
 
 import javax.validation.Valid;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-
 import lombok.RequiredArgsConstructor;
 import site.metacoding.blogv3.domain.user.User;
 import site.metacoding.blogv3.handler.ex.CustomException;
 import site.metacoding.blogv3.service.UserService;
 import site.metacoding.blogv3.web.dto.user.JoinReqDto;
-
 @RequiredArgsConstructor
 @Controller
 public class UserController {
-
     // DI
     private final UserService userService;
-
     @GetMapping("/login-form")
     public String loginForm() {
         return "/user/loginForm";
@@ -31,6 +29,12 @@ public class UserController {
     @GetMapping("/join-form")
     public String joinForm() {
         return "/user/joinForm";
+    }
+
+    @GetMapping("/api/user/username-same-check")
+    public ResponseEntity<?> usernameSameCheck(String username) {
+        boolean isNotSame = userService.유저네임중복체크(username); // true (같지 않다)
+        return new ResponseEntity<>(isNotSame, HttpStatus.OK);
     }
 
     @PostMapping("/join")
@@ -44,10 +48,8 @@ public class UserController {
             // 이부분에서 data리턴인지 html 리턴인지 이것만 구분해서 터트려줘!!
             throw new CustomException(errorMap.toString());
         }
-
         // 핵심 로직
         userService.회원가입(joinReqDto.toEntity());
-
         return "redirect:/login-form";
     }
 }
